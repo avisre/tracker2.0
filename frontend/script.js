b735a8e625934c1c7cb1ededad026ab78f77830a
avisre <90497762+avisre@users.noreply.github.com>
2024-09-22 17:24:13 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://stockportfolio.pro//api';
 let portfolioValueOverTime = []; // To track the portfolio value over time
 let stockAllocation = {}; // To track stock allocation by symbol
 
